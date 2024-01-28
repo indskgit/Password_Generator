71850a56af842cad687e6f4f54db0b55c24b71b6
Saurabh Kumar <skumarsaurbh2001@gmail.com>
2024-01-29 01:10:18 +0530

    Fixed bugs where inputs not generated perfectly

--- PasswordGenerator/script.js
+++ PasswordGenerator/script.js
@@ -70,11 +70,162 @@ function calcStrength() {
     if (symbolsCheck.checked) hasSym = true;
 
     if(hasUpper && hasLower && (hasNum || hasSym) && passLen >=8 ){
-        setIndicator("#0f0")
-    }else if(
-        (hasLower || hasUpper) &&
-        (hasNum || hasSym) && passLen >= 6
-    ){
+        setIndicator("#0f0");
+    }
+    else if((hasLower || hasUpper) &&(hasNum || hasSym) && passLen >= 6){
         setIndicator("#ff0");
-    }else setIndicator("#f00");
+    }
+    else setIndicator("#f00");
 }
+
+//copy the msg to ClipBoard
+async function copyContent() {
+   try {
+    await navigator.clipboard.writeText(passwordDisplay.value);
+    copyMsg.innerText = "Copied";
+   } catch (e) {
+    copyMsg.innerText = "failed";
+   }
+
+   //to make copy wala span visible
+   copyMsg.classList.add("active");
+
+   setTimeout(() => {
+    copyMsg.classList.remove("active");
+   }, 2000);
+};
+
+function shufflePassword(array){
+    //fisher Yates method
+    for(let i = array.length-1; i >0; i--){
+        const j = Math.floor(Math.random() * (i+1));
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    let str = "";
+    array.forEach((el) =>(str+= el));
+    return str;
+};
+
+// function handleCheckBoxChange() { // mine Code
+//     checkCount = 0;
+//     allCheckBox.forEach((checkbox) => {
+//         if(checkbox.checked) checkCount++;
+//     })
+
+//     //case where passlen < checkedCount
+//     if (passLen < checkCount) {
+//         passLen = checkCount;
+//         handleSlider();
+//     }
+// };
+
+function handleCheckBoxChange() {  // AI generated
+    let checkCount = 0;
+    const allCheckBox = Array.from(document.querySelectorAll('input[type="checkbox"]'));
+
+    allCheckBox.forEach((checkbox) => {
+        if (checkbox.checked) checkCount++;
+    });
+
+    if (passLen < checkCount) {
+        passLen = checkCount;
+        handleSlider();
+    }
+};
+
+// allCheckBox.forEach((checkbox)=>{
+//     checkbox.addEventListener('change', handleCheckBoxChange);
+// });
+
+const checkBoxArray = Array.from(allCheckBox);
+
+checkBoxArray.forEach((checkbox) => {
+    checkbox.addEventListener('change', handleCheckBoxChange);
+});
+
+//Adding EventListner to slider so as slider moves the display length will also update/change
+
+inputSlider.addEventListener('input', (e) =>{
+    passLen = e.target.value;
+    handleSlider();
+});
+
+//Adding EventListner to copyBtn
+
+copyBtn.addEventListener('click', () =>{
+        if (passwordDisplay.value)
+         //when display not null or not empty
+            copyContent();
+});
+
+//Adding EventListner to Generate Password Button
+generateBtn.addEventListener('click', ()=>{
+    // none of checkbox selected
+    if(checkCount ==0) return;
+    if(passLen < checkCount){
+        passLen = checkCount;
+        handleSlider();
+    }
+
+    //generate new password
+    //remove old password
+    password = "";
+
+    //AS checkboxes checked
+    /* if(uppercaseCheck.checked){
+        password+= generateUpperCase();
+    }
+
+    if(lowercaseCheck.checked){
+        password+= generateLowerCase();
+    }
+
+    if(numbersCheck.checked){
+        password+= generateRandomNumber();
+    }
+
+    if(symbolsCheck.checked){
+        password+= generateSymbol();
+    }
+    */
+
+    let funArr = []; //created Empty Appray to Store Functions
+
+    if(uppercaseCheck.checked){
+        funArr.push(generateUpperCase);
+    }
+
+    if(lowercaseCheck.checked){
+        funArr.push(generateLowerCase);
+    }
+
+    if(numbersCheck.checked){
+        funArr.push(generateRandomNumber);
+    }
+
+    if(symbolsCheck.checked){
+        funArr.push(generateSymbol);
+    }
+
+    //Now adding these above to function
+    for (let i = 0; i < funArr.length; i++) {
+         password+= funArr[i]();
+    }
+
+    // Other i.e Remaining Passwords means let password length is 10 and checke boxes checke = 4 then from first for loop it will add checkedOne function to funArray then randomly generate remaining i.e [10 - 4] = 6
+
+    for (let i = 0; i < (passLen - funArr.length); i++) {
+        let rndIndex = getRndInteger(0, funArr.length);
+        password+= funArr[rndIndex]();
+    }
+
+    //shuffle the password
+    password = shufflePassword(Array.from(password));
+
+    // update UI
+    passwordDisplay.value = password;
+    //show strength
+    calcStrength();
+});
