d49b98e73984d4c0f5ab70da0fcd489be5a543e3
Saurabh Kumar <skumarsaurbh2001@gmail.com>
2024-02-05 19:19:28 +0530

    css changes

--- PasswordGenerator/script.js
+++ PasswordGenerator/script.js
@@ -223,3 +223,14 @@ generateBtn.addEventListener('click', ()=>{
     //show strength
     calcStrength();
 });
+
+
+// navbar
+function myFunction() {
+    var x = document.getElementById("mynav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
